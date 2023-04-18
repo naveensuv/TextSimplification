23f35965049684610a8beb22971cf1d6fb6bc645
Naveen <naveen.senthil@zohocorp.com>
2023-04-18 21:40:27 +0530

    Completed Project

---
+++ src/unsupervisedstatistical/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unsupervisedstatistical;
+
+public class Main 
+{    
+    public static void main(String[] args) 
+    {                        
+        MainFrame cf=new MainFrame();
+        cf.setTitle("Main Frame");
+        cf.setVisible(true);
+        cf.setResizable(false);
+    }        
+}
+
